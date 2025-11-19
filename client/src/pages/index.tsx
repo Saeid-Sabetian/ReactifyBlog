a12fc5b042c430faee98e005c5eb9352457b7767
Saied Sabetian <saeidsabetian1997@gmail.com>
2025-11-19 14:31:58 +0330

    feature:add footer component

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import Header from '../components/Header';
 import { Outlet } from 'react-router-dom';
+import Footer from '../components/Footer';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Header />
       <main>
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 };
