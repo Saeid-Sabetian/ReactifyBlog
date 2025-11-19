772a3be6bf4b5a54479c6cbd40ca4559bd71244b
Saied Sabetian <saeidsabetian1997@gmail.com>
2025-11-19 11:13:04 +0330

    feature:Complete missing part of header component

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -8,24 +8,6 @@ const App: React.FC = () => {
 
       {/* Header Close */}
 
-      <div className="main-wrapper ">
-        <section className="page-title bg-1">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12 col-12">
-                <div className="block text-center">
-                  <span className="text-white">Our blog</span>
-                  <h1 className="text-capitalize mb-4 text-lg">Blog articles</h1>
-                  <ul className="list-inline">
-                    <li className="list-inline-item"><a href="index.html" className="text-white">Home</a></li>
-                    <li className="list-inline-item"><span className="text-white">/</span></li>
-                    <li className="list-inline-item"><a href="#" className="text-white-50">Our blog</a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
 
         <section className="section blog-wrap bg-gray">
           <div className="container">
@@ -196,7 +178,6 @@ const App: React.FC = () => {
             </div>
           </div>
         </footer>
-      </div>
     </>
   );
 };
